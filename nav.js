157cce175b7c978cde281ffeb9763a967133e510
THKok <40333402+THKok@users.noreply.github.com>
2026-06-20 15:12:52 +0200

    Add files via upload

--- nav.js
+++ nav.js
@@ -60,7 +60,7 @@ const NAV_ITEMS = [
       { label: 'Overzicht materiaal', label_en: 'Equipment overview', url: 'materiaal/index.html' },
       { label: 'Antistolling bij interventies', label_en: 'Anticoagulation in interventions', url: 'materiaal/antistolling.html' },
       { label: 'Angioset', label_en: 'Angio set',            url: 'materiaal/angioset.html' },
-      { label: 'Katheters', label_en: 'Catheters',           url: 'materiaal/katheters.html' },
+      { label: 'Sheaths & Katheters', label_en: 'Sheaths & Catheters', url: 'materiaal/sheaths-en-katheters.html' },
       { label: 'Draden', label_en: 'Guidewires',              url: 'materiaal/draden.html' },
     ]
   }
